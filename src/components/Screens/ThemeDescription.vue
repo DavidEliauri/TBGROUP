e7ecca28f89ac5ebc183c6792c25d1cb49a9500c
bourbakiii <gabisov03912@gmail.com>
2022-10-19 18:06:17 +0300

    Theme description screen

---
+++ src/components/Screens/ThemeDescription.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="screen theme-description-screen">
+    <p class="theme-description-screen__text lead_1">
+      В наших программах мы исходим из убеждения,
+      что каждый взрослый человек, независимо от
+      своего опыта, может прокачать креативное
+      мышление. Прямых работающих алгоритмов развития
+      креативного мышления не существует. Добиться
+      этого можно только комплексным и планомерным
+      подходом, развивая в себе целый набор качеств и
+      навыков.
+    </p>
+    <PostsSlider class="theme-description-screen__posts-slider"/>
+  </div>
+</template>
+
+<script setup>
+import PostsSlider from '@/components/Posts/Slider.vue';
+</script>
+
+<style lang='scss'>
+.theme-description-screen {
+  color: $GREEN;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  grid-gap: 94px;
+  @media screen and (max-width: $notebook_start) {
+    grid-gap: auto-calculate($notebook_start, $tablet, 94px, 47px);
+  }
+  @media screen and (max-width: $tablet) {
+    grid-gap: 40px;
+  }
+}
+</style>
--- src/pages/Theme.vue
+++ src/pages/Theme.vue
@@ -1,11 +1,13 @@
 <template>
   <main class="theme-page page">
-    <SupportScreen class='theme-page__support-screen screen'/>
+<!--    <SupportScreen class='theme-page__support-screen screen'/>-->
+    <ThemeDescriptionScreen class='theme-page__theme-description-screen wrapper-padding wrapper-maxwidth'/>
   </main>
 </template>
 
 <script setup>
-import SupportScreen from '@/components/Screens/Support.vue';
+// import SupportScreen from '@/components/Screens/Support.vue';
+import ThemeDescriptionScreen from '@/components/Screens/ThemeDescription.vue';
 </script>
 
 <style lang='scss'>
